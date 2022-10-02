758d95f85704a20d3c5665b031404e0b0ae198e6
anixsam <anixsamsajikdm@gmail.com>
2022-10-02 10:28:32 -0700

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavBar from './components/navbar';
+
+function App() {
+  return (
+    <div>
+      <NavBar>
+      </NavBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+  return <div className="navbar">
+
+  </div>;
+};
+
+export default NavBar;
